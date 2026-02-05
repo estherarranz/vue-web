14c2bdfb7fb498da829dd48e1e0998f734215e9d
Fernando Cordón <fcordon@pin.upv.es>
2026-02-05 09:59:54 +0100

    Configuración inicial

--- src/paginae/domus/Domus.vue
+++ src/paginae/domus/Domus.vue
@@ -1,6 +1,34 @@
+<script lang="ts" setup>
+import Button from '@/components/ui/button/Button.vue';
+
+
+</script>
 
 <template>
 
-<h1>Home page</h1>
+<div class="cappa">
+  <h1>Home page</h1>
+  <Button variant="destructive">Cick me</Button>
+  <Button class="viridis" size="lg" variant="tertiarius">Cick me</Button>
+</div>
 
 </template>
+
+<style scoped>
+.cappa {
+  margin: 20px;
+}
+
+h1 {
+  font-size: 24px;
+  margin-bottom: 12px;
+}
+
+Button {
+  margin-right: 12px;
+}
+
+.viridis {
+  padding: 0 100px;
+}
+</style>
