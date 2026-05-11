70e70fbbf60b38061e14bb9b5342fdf036f147d1
estherarranz <estherarranzzz@gmail.com>
2026-05-11 14:10:17 +0200

    earrati: página Circa

--- src/paginae/simpsons/Layout.vue
+++ src/paginae/simpsons/Layout.vue
@@ -1,35 +1,54 @@
 <script setup lang="ts">
+import LogoSimpsons from '@/LogoSimpsons.vue';
+
 
 </script>
-
-
 
 
 <template>
     <div class="grid grid-rows[auto_1fr_auto] min-h-screen">
 
-        <header class="bg-[#ffde00] w-full border-black">
+        <header class="bg-[#ffde00] w-full border border-black">
 
-       <RouterLink to="/simpsons/gallery">
-        Characters
-       </RouterLink>
+            <div class="flex flex-col md:flex-row items-center justify-between p-3 mx-auto max-w-[1400px] md:px-20">
+                 <LogoSimpsons estilus="w-[8rem] h-auto" />
 
-       <RouterLink to="/simpsons/about">
-        about
-       </RouterLink>
+            <div class="flex gap-6 pt-3">
+                <RouterLink to="/simpsons/gallery" active-class="font-bold">
+                    Characters
+                </RouterLink>
 
+                <RouterLink to="/simpsons/about"  active-class="font-bold">
+                    About
+                </RouterLink>
 
+                <Button variant="tertiarius" size="elongatis" class="bg-[#ee3133] hover:bg-[#ffde00]">
+                    <RouterLink to="/simpsons">
+                        Exit
+                    </RouterLink>
 
+                </Button>
+
+            </div>
+
+            </div>
+
+    
         </header>
        
-
         <main>
             <RouterView />
         </main>
 
-        <footer>
+        <footer class="text-center bg-[#ffde00] w-full border border-black py-6">
 
-            <h1>Footer</h1>
+            <div>
+	            <strong>All Rights Reserved</strong> - Oh Lord Matt Groening - 1989-2026
+            </div>
+
+            <small>
+                This app was done by Fernando Cordón
+            </small>
 
         </footer>
 
